8906f014f7304d4feffeb5b6eb236d3777b0019f
Devon Shigaki <devon.shigaki@freshcredit.com>
2024-09-10 17:21:28 -0700

    updated app.js and app.test.js

--- app.js
+++ app.js
@@ -1,12 +1,9 @@
 // app.js
 const express = require('express');
 const app = express();
-const PORT = 3000;
 
 app.get('/', (req, res) => {
   res.send('Hello World');
 });
 
-const server = app.listen(PORT);
-
-module.exports = { app, server };
+module.exports = app; // Export only the app
--- tests/app.test.js
+++ tests/app.test.js
@@ -1,8 +1,22 @@
 // tests/app.test.js
 const request = require('supertest');
-const app = require('../app');
+const app = require('../app'); // Import only the app
+
+let server;
 
 describe('GET /', () => {
+  // Start the server before each test
+  beforeAll((done) => {
+    server = app.listen(3000, () => {
+      done();
+    });
+  });
+
+  // Close the server after all tests
+  afterAll((done) => {
+    server.close(done);
+  });
+
   it('should return Hello World', async () => {
     const res = await request(app).get('/');
     expect(res.statusCode).toEqual(200);
